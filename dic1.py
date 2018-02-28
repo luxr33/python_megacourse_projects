79ca826e8477db690b87a7481c4de9aedc1d6e0c
luxr33 <luxr33@gmail.com>
2018-02-28 13:12:26 +0100

    Update dic1.py

--- dic1.py
+++ dic1.py
@@ -2,7 +2,22 @@ import json
 from difflib import get_close_matches
 data = json.load(open("data.json"))
 j=0
+
+
+def checkcity(word):
+
+
+    if word[0] == word[0].upper() and word[1:] == word[1:].lower():
+        if word in data:
+            return data[word]
+        else:
+            return 0
+
 def translate(word):
+
+
+    checkcity(word)
+    word= word.lower()
     if word in data:
         global j
         j = 1
@@ -11,6 +26,8 @@ def translate(word):
         return similar(word)
 
 def similar(word):
+    
+    
     if len(get_close_matches(word, data.keys())) > 0:
         check = input("did you meant %s ? y for yes, n for nope  " % get_close_matches(word, data.keys())[0]).upper()
         if check == "Y":
@@ -24,8 +41,10 @@ def similar(word):
 
     else:
         return "the word does not exist, recheck"
+    
+    
 while True:
-    word = input("tell me the word, type exit1 to quit: ").lower()
+    word = input("tell me the word, type exit1 to quit: ")
     if word== "exit1":
         break
     out = translate(word)
